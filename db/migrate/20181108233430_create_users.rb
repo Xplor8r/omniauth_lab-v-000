4e38dcced58a3d8c48af84dfa7201fbc9b48ce12
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-08 23:36:51 +0000

    automatically backed up by learn

--- db/migrate/20181108233430_create_users.rb
+++ db/migrate/20181108233430_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.name :string
-      t.password_digest :string
+      t.string :name
+      t.string :password_digest
 
       t.timestamps null: false
     end
